dc2fa3da778ffd93c2bc9416a38a3778a02bbb73
Aadil Bachh <aadilbachh@icloud.com>
2020-04-21 07:27:07 +0100

    initial xcode project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Aadil Bachh on 21/04/2020.
+//  Copyright © 2020 Aadil Bachh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
